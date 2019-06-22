fd0f7bb27b9192c841cada4c29522346d37df7b5
Mary <m5fgn@outlook.com>
2019-06-22 11:20:48 +0100

    Initial Upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function commoncontent () {
+    let date = new Date();
+    document.getElementById('txt_copyright').innerHTML = "Mary Fagan | Copyright "+date.getFullYear();
+    document.getElementById('txt_links').innerHTML = '    <div class="links"> <a href="https://codepen.io/m5fgn/"><i class="fab fa-codepen"></i></a><a href="https://github.com/M5FGN"><i class="fab fa-github"></i></a><a href="https://twitter.com/m5fgn"><i class="fab fa-twitter"></i></a>    <a href="#"><i class="far fa-address-card"></i></a></div>';
+    }
+
+    
+
