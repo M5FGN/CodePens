6567b2666bde9c2fb99dcfb57a43ea0e59054eab
m5fgn <m5fgn@outlook.com>
2021-02-09 17:56:17 +0000

    Update Color Changing Background to a single file.

--- Color_Changing_Background/color_change.js
+++
@@ -1,5 +0,0 @@
-function changeColor() {
-    console.log("clicked");
-    document.body.style.backgroundColor =
-    'rgb('+ Math.round(Math.random()*255) + ',' + Math.round(Math.random()*255) + ',' + Math.round(Math.random()*255)+ ')';
-}
