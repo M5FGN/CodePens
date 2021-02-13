8ac14530a78b6bc7c25fe0d8531a56acb3b62ff6
M5FGN <33698169+M5FGN@users.noreply.github.com>
2021-02-13 21:08:30 +0000

    Removing Old Code Pen Files

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function commoncontent () {
-    let date = new Date();
-    document.getElementById('txt_copyright').innerHTML = "Mary Fagan | Copyright "+date.getFullYear();
-    document.getElementById('txt_links').innerHTML = '    <div class="links"> <a href="https://codepen.io/m5fgn/" target="_blank"><i class="fab fa-codepen"></i></a><a href="https://github.com/M5FGN" target="_blank"><i class="fab fa-github"></i></a><a href="https://twitter.com/m5fgn" target="_blank"><i class="fab fa-twitter"></i></a>    <a href="#" target="_blank"><i class="far fa-address-card"></i></a></div>';
-    }
-
-    
-
