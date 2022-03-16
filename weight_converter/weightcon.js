c44adc37d4db38769250c0ec570d547f3a980444
m5fgn <m5fgn@outlook.com>
2022-03-16 07:45:04 +0000

    Update Calculation

--- weight_converter/weightcon.js
+++ weight_converter/weightcon.js
@@ -1,11 +1,7 @@
 function calculation() {
-    // console.log("Changed");
     let inputvalue = document.getElementById("inputvalue").value;
     let inputunit = document.getElementById("inputunit").value;
     let outputunit = document.getElementById("outputunit").value;
-    // console.log(inputvalue);
-    // console.log(inputunit);
-    // console.log(outputunit);
 
     if (inputunit == "lbs" && outputunit == "lbs") {
         outputvalue = inputvalue;
@@ -18,7 +14,7 @@ function calculation() {
         return outputvalue;
     }
     else if(inputunit == "kgs" && outputunit == "lbs") {
-        outputvalue = (inputvalue * 2.205).toFixed(3);
+        outputvalue = (inputvalue * 2.205).toFixed(0);
         document.getElementById("outputvalue").value = outputvalue;
         return outputvalue;
     }
@@ -27,6 +23,4 @@ function calculation() {
         document.getElementById("outputvalue").value = outputvalue;
         return outputvalue;
     }
-
-
 }
